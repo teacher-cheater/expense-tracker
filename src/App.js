ee54a81c2aec5894a4d7f78db5ea074bdc5d924b
teacher-cheater <johnnykayfun@gmail.com>
2024-09-10 17:23:48 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [formData, setFormData] = useState({description: '', date: '', category: '', amount: ''});
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormData({description: '', date: '', category: '', amount: ''});
+        console.log(formData)
+    };
+
+
+    // const calculateBricksCount = (width, height) => {
+    //     let rows = height / 5;
+    //     let bricksL = width / 60;
+    //     let resBrickL = 0;
+    //     let resBrickM = 0;
+    //     let resBrickS = 0;
+    //
+    //     for (let i = 1; i < rows; i++) {
+    //         if (i % 3 === 1) {
+    //             resBrickL += bricksL
+    //         } else if (i % 3 === 2) {
+    //             resBrickM += 1;
+    //             resBrickS += 1;
+    //             resBrickL += bricksL - 2
+    //         } else if (i % 3 === 0){
+    //             resBrickM += 1;
+    //             resBrickS += 1;
+    //             resBrickL += bricksL - 2
+    //         }
+    //     }
+    //
+    //     console.log(resBrickL)
+    //     console.log(resBrickM)
+    //     console.log(resBrickS)
+    //
+    //
+    // }
+    //
+    // console.log(calculateBricksCount(180, 45)) // '21L6M6S'
+    // // console.log(calculateBricksCount(60, 5)) // '1L'
+
+
+    return (
+        <div className="App">
+            <ExpenseForm handleSubmit={handleSubmit} handleChange={handleChange} formData={formData}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ExpenseForm/ExpenseForm.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import s from './ExpenseForm.module.css';
+import car from '../../images/icons/car.svg';
+import bag from '../../images/icons/bag.svg';
+import gameboy from '../../images/icons/gameboy.svg';
+import house from '../../images/icons/house.svg';
+import more from '../../images/icons/more.svg';
+import teacher from '../../images/icons/teacher.svg';
+
+const ExpenseForm = ({handleSubmit, handleChange, formData}) => {
+    return (
+        <div className={s.expenses}>
+            <h2 className={s.title}>Новый расход</h2>
+            <form onSubmit={handleSubmit}>
+                <label className={s.wrap}>
+                    <h4 className={s.subtitle}>Описание</h4>
+                    <input name="description" type="text" value={formData.description} onChange={handleChange}
+                           required/>
+                </label>
+                <div className={s.wrapper}>
+                    <h4 className={s.subtitle}>Категория</h4>
+                    <input
+                        type="radio"
+                        id="meal"
+                        name="category"
+                        value="meal"
+                        checked={formData.category === 'meal'}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="meal" className={s.categoryLabel}>
+                        <img src={bag} alt=""/>
+                        Еда
+                    </label>
+
+                    <input
+                        type="radio"
+                        id="transport"
+                        name="category"
+                        value="transport"
+                        checked={formData.category === 'transport'}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="transport" className={s.categoryLabel}>
+                        <img src={car} alt=""/>
+                        Транспорт
+                    </label>
+
+                    <input
+                        type="radio"
+                        name="category"
+                        value="housing"
+                        id="housing"
+                        checked={formData.category === 'housing'}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="housing" className={s.categoryLabel}>
+                        <img src={house} alt=""/>
+                        Жилье
+                    </label>
+
+                    <input
+                        type="radio"
+                        name="category"
+                        value="entertainments"
+                        id="entertainments"
+                        checked={formData.category === 'entertainments'}
+                        onChange={handleChange}
+                    />
+
+                    <label htmlFor="entertainments" className={s.categoryLabel}>
+                        <img src={gameboy} alt=""/>
+                        Развлечения
+                    </label>
+
+                    <input
+                        type="radio"
+                        name="category"
+                        value="education"
+                        id="education"
+                        checked={formData.category === 'education'}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="education" className={s.categoryLabel}>
+                        <img src={teacher} alt=""/>
+                        Образование
+                    </label>
+
+                    <input
+                        type="radio"
+                        name="category"
+                        value="more"
+                        id="more"
+                        checked={formData.category === 'more'}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="more" className={s.categoryLabel}>
+                        <img src={more} alt=""/>
+                        Другое
+                    </label>
+                </div>
+                <h4 className={s.subtitle}>Дата</h4>
+                <label className={s.wrap}>
+                    <input name="date" type="text" value={formData.date} onChange={handleChange} required/>
+                </label>
+                <label className={s.wrap}>
+                    <h4 className={s.subtitle}>Сумма</h4>
+                    <input name="amount" type="number" value={formData.amount} onChange={handleChange} required/>
+                </label>
+                <button className={s.btn} type="submit">Добавить новый расход</button>
+            </form>
+        </div>
+    );
+};
+
+export default ExpenseForm;
